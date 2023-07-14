f02e9b9bd0ac1183ba88c29429bf4018a884dfdb
Jeremy-dev94 <jgaudry1294@gmail.com>
2023-07-14 16:09:29 +0200

    2éme_commit

--- shop_projet.js
+++ shop_projet.js
@@ -1,37 +1,35 @@
-// Exemple de données de produits
-var products = [
-  { name: "Produit 1", price: 9.99, image: "produit1.jpg" },
-  { name: "Produit 2", price: 19.99, image: "produit2.jpg" },
-  { name: "Produit 3", price: 14.99, image: "produit3.jpg" },
+// Variables globales
+var currentImageIndex = 0;
+var images = [
+  "3cebb4605e6ecab31bd7540dd686bafc.jpg",
+  "d375469.jpg",
+  "mur-pierre-seche.jpg"
 ];
 
-// Fonction pour afficher les produits
-function displayProducts() {
-  var productsList = document.getElementById("products-list");
-  
-  products.forEach(function(product) {
-    var productElement = document.createElement("div");
-    productElement.classList.add("product");
-    
-    var imageElement = document.createElement("img");
-    imageElement.src = product.image;
-    productElement.appendChild(imageElement);
-    
-    var nameElement = document.createElement("h2");
-    nameElement.textContent = product.name;
-    productElement.appendChild(nameElement);
-    
-    var priceElement = document.createElement("p");
-    priceElement.textContent = "$" + product.price.toFixed(2);
-    productElement.appendChild(priceElement);
-    
-    var addButton = document.createElement("button");
-    addButton.textContent = "Ajouter au panier";
-    productElement.appendChild(addButton);
-    
-    productsList.appendChild(productElement);
+// Obtient la référence vers les éléments du carrousel
+var carousel = document.querySelector('.carousel');
+var carouselImages = Array.from(carousel.querySelectorAll('.carousel-image'));
+
+// Fonction pour afficher l'image active
+function showActiveImage() {
+  carouselImages.forEach(function(image) {
+    image.classList.remove('active');
   });
+  carouselImages[currentImageIndex].classList.add('active');
 }
 
-// Appel de la fonction pour afficher les produits lors du chargement de la page
-window.onload = displayProducts;
+// Fonction pour changer à l'image précédente
+function showPrevImage() {
+  currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
+  showActiveImage();
+}
+
+// Fonction pour changer à l'image suivante
+function showNextImage() {
+  currentImageIndex = (currentImageIndex + 1) % images.length;
+  showActiveImage();
+}
+
+// Ajoute des gestionnaires d'événements aux boutons
+prevButton.addEventListener('click', showPrevImage);
+nextButton.addEventListener('click', showNextImage);
